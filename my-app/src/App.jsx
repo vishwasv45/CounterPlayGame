7c450e9a809b46af48b79f5f2741ecaa59ebb3b6
vishwas <vishwasmyya19@gmail.com>
2024-09-24 16:26:37 +0530

    first commit

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Home from './Components/Homepage'
+function App() {
+  
+     return(<>
+     <Home/>
+     </>)
+}
+export default App
